d0148231443070894cb9eed579552e79fe682f9e
chifek <chifek@gmail.com>
2021-02-13 18:44:07 +0300

    Added vibration and sensorListener

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,21 +1,36 @@
 package com.example.diceroller
 
-import android.hardware.Sensor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.widget.ImageView
 import android.widget.Toast
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import kotlin.math.sqrt
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private var sensorManager: SensorManager? = null
+    private var acceleration = 0f
+    private var currentAcceleration = 0f
+    private var lastAcceleration = 0f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener {
-            rollDice()
-            Toast.makeText(this, "Рус теперь мобильный разработчик :P", Toast.LENGTH_SHORT).show()
-        }
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        Objects.requireNonNull(sensorManager)!!.registerListener(sensorListener, sensorManager!!
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL)
+        acceleration = 10f
+        currentAcceleration = SensorManager.GRAVITY_EARTH
+        lastAcceleration = SensorManager.GRAVITY_EARTH
+        Toast.makeText(applicationContext, "Встряхни, телефон", Toast.LENGTH_SHORT).show()
     }
 
     private fun rollDice() {
@@ -29,5 +44,50 @@ class MainActivity : AppCompatActivity() {
         }
         val diceImage: ImageView = findViewById(R.id.roll_image)
         diceImage.setImageResource(drawableResource)
+    }
+
+    private val sensorListener: SensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+            lastAcceleration = currentAcceleration
+            currentAcceleration = sqrt((x * x + y * y + z * z).toDouble()).toFloat()
+            val delta: Float = currentAcceleration - lastAcceleration
+            acceleration = acceleration * 0.9f + delta
+            if (acceleration > 21) {
+                rollDice()
+                vibratePhone(250)
+            }
+
+            if (acceleration > 50) {
+                Toast.makeText(applicationContext, "Сейчас блевану", Toast.LENGTH_SHORT).show()
+            }
+
+            if (acceleration > 13 && acceleration < 20) {
+                vibratePhone(100)
+            }
+        }
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+    }
+
+    override fun onResume() {
+        sensorManager?.registerListener(sensorListener, sensorManager!!.getDefaultSensor(
+                Sensor .TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL
+        )
+        super.onResume()
+    }
+
+    override fun onPause() {
+        sensorManager!!.unregisterListener(sensorListener)
+        super.onPause()
+    }
+
+    fun vibratePhone(ml: Int) {
+        val v = (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(ml.toLong(),
+                    VibrationEffect.DEFAULT_AMPLITUDE))
+        }
     }
 }
